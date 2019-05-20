8eb5d488d3521dd07630ab8ae86aa314021c301f
yosuahohoho <yosuahohoho@gmail.com>
2019-05-20 18:57:15 +0700

    try render markdown document container from app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="document-container">
+        <div className="document-markdown">
+          <textarea placeholder="This is your markdown"/>
+        </div>
+        <div className="document-preview">
+          <textarea placeholder="This is your markdown preview"/>
+        </div>        
+      </section>
     </div>
   );
 }
