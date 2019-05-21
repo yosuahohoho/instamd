39aa5e365a36bdca8d9c7ee6d79f26a414e343f8
yosuahohoho <yosuahohoho@gmail.com>
2019-05-21 15:42:40 +0700

    add: Footer component and styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navbar from './components/Navbar';
 import Document from './components/Document';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Document />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>2019&copy;yosuahohoho</small>
+    </footer>
+  )
+}
+
+export default Footer;
