6584974e84e105e5cc97dfb018e55b0512f729db
yosuahohoho <yosuahohoho@gmail.com>
2019-07-08 11:43:07 +0700

    Updates

---
+++ precache-manifest.df8b17075e0b9a046d41a154a2db3b6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c8e96837ddcd6662014f4ecd690243",
+    "url": "/instamd/index.html"
+  },
+  {
+    "revision": "96089b165b695a9c788c",
+    "url": "/instamd/static/css/main.8d5b47c7.chunk.css"
+  },
+  {
+    "revision": "923166e47361cba7147c",
+    "url": "/instamd/static/js/2.792504fd.chunk.js"
+  },
+  {
+    "revision": "96089b165b695a9c788c",
+    "url": "/instamd/static/js/main.19a732f2.chunk.js"
+  },
+  {
+    "revision": "6868f5f88dbcb262862e",
+    "url": "/instamd/static/js/runtime~main.26ed1171.js"
+  }
+]);
