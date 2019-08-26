86447ca96b6a7f719f282aa7a90d1a0207944437
yosuahohoho <yosuahohoho@gmail.com>
2019-08-26 11:55:33 +0700

    Change class name to document__name

--- src/components/Document/DocumentName.js
+++ src/components/Document/DocumentName.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const DocumentName = ({ name, handleChangeName }) => {
   return (
-    <div className='document__title'>
-      <label htmlFor='title'>Document Name</label>
+    <div className='document__name'>
+      <label htmlFor='name'>Document Name</label>
       <input
         type='text'
         value={name}
         onChange={handleChangeName}
-        name='title'
+        name='name'
         placeholder='untitled.md'
       />
     </div>
